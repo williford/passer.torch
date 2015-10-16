4771d58d6acf3bb0c366c8785482705f448d5950
Jonathan <jonathan@Williford-VandC.nin>
2015-10-16 10:36:03 +0200

    Change tocpumodel to tocpu. Update rockspec.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# passer
+passer
+======
+
 Converts GPU torch7 models to CPU equivalent models.
+The current version is very limited in what it is able to convert, although the code is easy to extend.
+
+Installation
+============
+To install via luarocks, run:
+
+    luarocks install --server=http://luarocks.org/manifests/williford passer
+
+Or clone the repository:
+
+    git clone git@github.com:williford/passer.torch.git
+
+and then copy the passer.lua file to your working folder.
+
+In order to use:
+
+    local passer = require 'passer'
+
+
+Usage
+=====
+
+The conversion needs to be done on a system that has a GPU (that is able to load the required model).
+
+```lua
+require 'nn';
+require 'cudnn';
+
+-- do not put "local" if in interactive session!
+local passer = require 'passer'
+
+model = torch.load('/path/to/model_xx.t7')
+cpu_model = passer.tocpu(model)
+torch.save('cpu_model_xx.t7')
+```
--- passer.lua
+++ passer.lua
@@ -1,14 +1,14 @@
 -- Modified from Michael Partheil
 -- (https://groups.google.com/forum/#!topic/torch7/i8sJYlgQPeA)
 local passer = {
-  _VERSION = 'passer.torch 0.0.0',
+  _VERSION = 'passer.torch 0.0.1',
   _URL     = 'https://github.com/williford/passer.torch',
   _DESCRIPTION = 'Converts GPU torch models to CPU equivalent versions',
   _LICENSE = 'MIT'
 }
 
-function passer.replace_modules(net, orig_class_name, replacer)
-  local nodes, container_nodes = net:findModules(orig_class_name)
+function passer.replace_modules(model, orig_class_name, replacer)
+  local nodes, container_nodes = model:findModules(orig_class_name)
   for i = 1, #nodes do
     for j = 1, #(container_nodes[i].modules) do
       if container_nodes[i].modules[j] == nodes[i] then
@@ -19,18 +19,25 @@ function passer.replace_modules(net, orig_class_name, replacer)
   end
 end
 
-function passer.tocpumodel(net)
-  local net_cpu = net:clone():float()
+function passer.tocpu(model)
+  local cpu_model = model:clone():float()
 
-  passer.replace_modules(net_cpu, 'cudnn.SpatialConvolution', 
+  passer.replace_modules(cpu_model, 'cudnn.SpatialConvolution', 
     function(orig_mod)
-      local cpu_mod = nn.SpatialConvolutionMM(orig_mod.nInputPlane, orig_mod.nOutputPlane,
-          orig_mod.kW, orig_mod.kH, orig_mod.dW, orig_mod.dH, orig_mod.padW, orig_mod.padH)
+      local cpu_mod = nn.SpatialConvolutionMM(
+        orig_mod.nInputPlane,
+        orig_mod.nOutputPlane,
+        orig_mod.kW,
+        orig_mod.kH,
+        orig_mod.dW,
+        orig_mod.dH,
+        orig_mod.padW,
+        orig_mod.padH)
       cpu_mod.weight:copy(orig_mod.weight)
       cpu_mod.bias:copy(orig_mod.bias)
       return cpu_mod
     end)
-  passer.replace_modules(net_cpu, 'cudnn.SpatialMaxPooling',
+  passer.replace_modules(cpu_model, 'cudnn.SpatialMaxPooling',
     function(orig_mod)
       local cpu_mod = nn.SpatialMaxPooling(
         orig_mod.kW,
@@ -42,9 +49,9 @@ function passer.tocpumodel(net)
       return cpu_mod
     end)
 
-  passer.replace_modules(net_cpu, 'cudnn.ReLU', function() return nn.ReLU() end)
+  passer.replace_modules(cpu_model, 'cudnn.ReLU', function() return nn.ReLU() end)
 
-  return net_cpu
+  return cpu_model
 end
 
 return passer
